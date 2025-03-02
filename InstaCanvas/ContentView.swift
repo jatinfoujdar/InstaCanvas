8543236f438ee543039b9bef547ba9a2bb77f9c7
jatin foujdar <jatin321001@gmail.com>
2025-03-02 12:33:00 +0530

    Initial Commit

---
+++ InstaCanvas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstaCanvas
+//
+//  Created by jatin foujdar on 02/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstaCanvas/InstaCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCanvasApp.swift
+//  InstaCanvas
+//
+//  Created by jatin foujdar on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct InstaCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
